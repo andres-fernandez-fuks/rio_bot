ee6eb6db6557fd370a34fe146b5ac82c1c5ae6ad
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-11-28 19:40:13 -0300

    Agrego validacion de url en ApiFiubak

--- app/api_fiubak.rb
+++ app/api_fiubak.rb
@@ -3,6 +3,8 @@ require 'uri'
 
 class ApiFiubak
   def initialize(url)
+    raise StandardError unless url =~ URI::DEFAULT_PARSER.make_regexp
+
     @url = url
   end
 end
--- spec/api_fiubak_spec.rb
+++ spec/api_fiubak_spec.rb
@@ -6,4 +6,8 @@ describe 'ApiFiubak' do
   it 'deberia crearse con una url valida' do
     expect { ApiFiubak.new('http://rio.api.com/') }.not_to raise_error
   end
+
+  it 'deberia levantar error al crearse con url invalida' do
+    expect { ApiFiubak.new('una url invalida') }.to raise_error StandardError
+  end
 end
