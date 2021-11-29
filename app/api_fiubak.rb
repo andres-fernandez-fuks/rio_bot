a77c6bee2a9c3fa2971b1a5a2d66778242d780a1
Andres <andyfer91@gmail.com>
2021-11-29 16:14:31 -0300

    agrego funcionalidad de consultar usuario

--- app/api_fiubak.rb
+++ app/api_fiubak.rb
@@ -1,5 +1,8 @@
 require 'faraday'
 require 'uri'
+require 'byebug'
+
+BASE_URL = 'http://rio.api.com'.freeze
 
 class ApiFiubak
   def initialize(url)
@@ -11,5 +14,10 @@ class ApiFiubak
   def registrar_usuario(id_telegram, email, nombre)
     body = { id_telegram: id_telegram, email: email, nombre: nombre }.to_json
     Faraday.post("#{@url}/usuarios", body)
+  end
+
+  def consultar_usuario(id_telegram)
+    body = { id_telegram: id_telegram }
+    Faraday.get("#{@url}/usuarios", body)
   end
 end
--- spec/api_fiubak_spec.rb
+++ spec/api_fiubak_spec.rb
@@ -15,11 +15,18 @@ describe 'ApiFiubak' do
     expect { ApiFiubak.new('una url invalida') }.to raise_error StandardError
   end
 
-  it 'registrar usuario deberia enviar POST a /user con id de telegram, email y nombre' do
+  it 'registrar usuario deberia enviar POST a /usuarios con id de telegram, email y nombre' do
     stub = stub_request(:post, 'http://rio.api.com/usuarios')
            .with(body: { 'id_telegram': id_telegram, 'email': email, 'nombre': nombre }.to_json)
            .to_return status: 201
     ApiFiubak.new('http://rio.api.com').registrar_usuario(id_telegram, email, nombre)
+    expect(stub).to have_been_requested
+  end
+
+  it 'consultar por un usuario deberia enviar GET a /user con id de telegram, email y nombre' do
+    stub = stub_request(:get, 'http://rio.api.com/usuarios?id_telegram=' + id_telegram)
+           .to_return status: 200 | 404
+    ApiFiubak.new('http://rio.api.com').consultar_usuario(id_telegram)
     expect(stub).to have_been_requested
   end
 end
