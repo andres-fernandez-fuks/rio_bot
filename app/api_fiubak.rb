2cef9965fafe5eecd411fba0d6a9818d85190295
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-11-30 12:29:57 -0300

    renombro propiedad de ApiFiubak a @base_url

--- app/api_fiubak.rb
+++ app/api_fiubak.rb
@@ -5,20 +5,20 @@ require 'byebug'
 BASE_URL = 'http://rio.api.com'.freeze
 
 class ApiFiubak
-  def initialize(url)
-    raise StandardError unless url =~ URI::DEFAULT_PARSER.make_regexp
+  def initialize(base_url)
+    raise StandardError unless base_url =~ URI::DEFAULT_PARSER.make_regexp
 
-    @url = url
+    @base_url = base_url
   end
 
   def registrar_usuario(id_telegram, email, nombre)
     body = { id_telegram: id_telegram, email: email, nombre: nombre }.to_json
-    Faraday.post("#{@url}/usuarios", body)
+    Faraday.post("#{@base_url}/usuarios", body)
   end
 
   def consultar_usuario(id_telegram)
     body = { id_telegram: id_telegram }
-    Faraday.get("#{@url}/usuarios", body)
+    Faraday.get("#{@base_url}/usuarios", body)
   end
 
   def esta_registrado?(id_telegram)
