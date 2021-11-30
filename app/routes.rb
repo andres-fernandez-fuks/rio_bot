cf3f34bb0cf5430c585d313ebf11e1eb43e556d8
Andres <andyfer91@gmail.com>
2021-11-30 17:01:51 -0300

    elimino comandos y tests que venian por defecto

--- app/routes.rb
+++ app/routes.rb
@@ -6,28 +6,12 @@ require_relative 'api_fiubak'
 class Routes
   include Routing
 
-  on_message '/start' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{message.from.first_name}")
-  end
-
-  on_message_pattern %r{/say_hi (?<name>.*)} do |bot, message, args|
-    bot.api.send_message(chat_id: message.chat.id, text: "Hola, #{args['name']}")
-  end
-
   on_message_pattern %r{/registro (?<nombre_usuario>.*),(?<mail>.*)} do |bot, message, args|
     nombre_usuario = args['nombre_usuario']
     mail = args['mail']
     id_telegram = message.from.id
     respuesta = ApiFiubak.new(ENV['API_URL']).registrar_usuario(id_telegram, mail, nombre_usuario)
     bot.api.send_message(chat_id: message.chat.id, text: "Bienvenido #{nombre_usuario}, tu email es #{mail}") if respuesta.status == 201
-  end
-
-  on_message '/stop' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: "Chau, #{message.from.username}")
-  end
-
-  on_message '/time' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: "La hora es, #{Time.now}")
   end
 
   on_message '/help' do |bot, message|
@@ -45,27 +29,9 @@ class Routes
     end
   end
 
-  on_message '/busqueda_centro' do |bot, message|
-    kb = [
-      Telegram::Bot::Types::KeyboardButton.new(text: 'Compartime tu ubicacion', request_location: true)
-    ]
-    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-    bot.api.send_message(chat_id: message.chat.id, text: 'Busqueda por ubicacion', reply_markup: markup)
-  end
-
-  on_location_response do |bot, message|
-    response = "Ubicacion es Lat:#{message.location.latitude} - Long:#{message.location.longitude}"
-    puts response
-    bot.api.send_message(chat_id: message.chat.id, text: response)
-  end
-
   on_response_to '¿Con qué necesita ayuda?' do |bot, message|
     response = Opciones::OpcionUsuarioRegistrado.handle_response message.data
     bot.api.send_message(chat_id: message.message.chat.id, text: response)
-  end
-
-  on_message '/version' do |bot, message|
-    bot.api.send_message(chat_id: message.chat.id, text: Version.current)
   end
 
   default do |bot, message|
