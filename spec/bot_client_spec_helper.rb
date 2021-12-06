11eef953bf33d9e45d6ee16d4633f348187bbe44
Manuel <manuelsturla@gmail.com>
2021-12-06 17:32:34 -0300

    Refactor bot_client_spec

---
+++ spec/bot_client_spec_helper.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+require 'webmock/rspec'
+# Uncomment to use VCR
+# require 'vcr_helper'
+# rubocop:disable Metrics/LineLength
+require "#{File.dirname(__FILE__)}/../app/bot_client"
+
+def when_i_send_text(token, message_text)
+  body = { "ok": true, "result": [{ "update_id": 693_981_718,
+                                    "message": { "message_id": 11,
+                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
+                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                                                 "date": 1_557_782_998, "text": message_text,
+                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+
+  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
+    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+end
+
+def when_i_send_keyboard_updates(token, message_text, inline_selection)
+  body = {
+    "ok": true, "result": [{
+      "update_id": 866_033_907,
+      "callback_query": { "id": '608740940475689651', "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
+                          "message": {
+                            "message_id": 626,
+                            "from": { "id": 715_612_264, "is_bot": true, "first_name": 'fiuba-memo2-prueba', "username": 'fiuba_memo2_bot' },
+                            "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                            "date": 1_595_282_006,
+                            "text": message_text,
+                            "reply_markup": {
+                              "inline_keyboard": [
+                                [{ "text": 'Cómo registrar mi auto', "callback_data": '1' }],
+                                [{ "text": 'Cómo buscar publicaciones', "callback_data": '2' }],
+                                [{ "text": 'Cómo aceptar una oferta', "callback_data": '3' }]
+                              ]
+                            }
+                          },
+                          "chat_instance": '2671782303129352872',
+                          "data": inline_selection }
+    }]
+  }
+
+  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
+    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+end
+
+def then_i_get_text(token, message_text)
+  body = { "ok": true,
+           "result": { "message_id": 12,
+                       "from": { "id": 715_612_264, "is_bot": true, "first_name": 'fiuba-memo2-prueba', "username": 'fiuba_memo2_bot' },
+                       "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                       "date": 1_557_782_999, "text": message_text } }
+
+  stub_request(:post, "https://api.telegram.org/bot#{token}/sendMessage")
+    .with(
+      body: { 'chat_id' => '141733544', 'text' => message_text }
+    )
+    .to_return(status: 200, body: body.to_json, headers: {})
+end
+
+def then_i_get_keyboard_message(token, message_text)
+  body = { "ok": true,
+           "result": { "message_id": 12,
+                       "from": { "id": 715_612_264, "is_bot": true, "first_name": 'fiuba-memo2-prueba', "username": 'fiuba_memo2_bot' },
+                       "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                       "date": 1_557_782_999, "text": message_text } }
+
+  stub_request(:post, "https://api.telegram.org/bot#{token}/sendMessage")
+    .with(
+      body: { 'chat_id' => '141733544',
+              'reply_markup' => '{"inline_keyboard":[[{"text":"Cómo registrar mi auto","callback_data":"1"}],[{"text":"Cómo buscar publicaciones","callback_data":"2"}],[{"text":"Cómo aceptar una oferta","callback_data":"3"}]]}',
+              'text' => message_text }
+    )
+    .to_return(status: 200, body: body.to_json, headers: {})
+end
+# rubocop:enable Metrics/LineLength
